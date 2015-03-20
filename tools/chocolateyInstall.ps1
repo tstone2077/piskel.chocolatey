015ac6845b9008361271b0e2ac06d5c921024157
Thurston Stone <tstone2077@gmail.com>
2015-03-20 09:16:10 -0500

    Renaming piskel exe so it doesn't have the version number in the name.
    This will allow you to run "piskel" on the command line instead of:
    "Piskel-0.4.2"

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,3 +3,6 @@ $url = 'https://drive.google.com/uc?export=download&id=0BwPjxqZA0YlRNXRzZXlrc2F1
 
 # download and unpack a zip file
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$src = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "piskel\\Piskel-0.4.2.exe"
+Rename-Item -Path $src -newName "Piskel.exe"
+
